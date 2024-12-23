eec9f349b4081d625802a41503fd4934836d6023
Miguel Ramos <return.infox@gmail.com>
2024-12-23 10:06:02 -0300

    entidad WebSite terminada

---
+++ src/main/java/com/kibernumacademy/company/entities/Category.java
@@ -0,0 +1,11 @@
+package com.kibernumacademy.company.entities;
+
+public enum Category {
+  SOCIAL_NETWORK,
+  SERVICES,
+  STREAMING,
+  CLOUD_COMPUTING,
+  DEVICES,
+  EDUCATION,
+  NONE
+}
--- src/main/java/com/kibernumacademy/company/entities/WebSite.java
+++ src/main/java/com/kibernumacademy/company/entities/WebSite.java
@@ -1,9 +1,9 @@
 package com.kibernumacademy.company.entities;
 
-import java.util.Locale.Category;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,11 +18,20 @@ public class WebSite {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @Column(nullable = false, unique = true)
   private String name;
+
+  @Column(length = 255)
   private String url;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 32)
   private Category category;
+
+  @Column(columnDefinition = "TEXT")
   private String description;
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name="id_company", nullable= false)
   private Company company;
